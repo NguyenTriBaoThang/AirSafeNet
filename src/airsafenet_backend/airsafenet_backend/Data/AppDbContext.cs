6cd94fa767398a3568071dd3a8d339b7c41a6b7e
NguyenTriBaoThang <nguyentribaothang@gmail.com>
2026-04-05 00:39:18 +0700

    feat(data): Add AppDbContext and database setup

---
+++ src/airsafenet_backend/airsafenet_backend/Data/AppDbContext.cs
@@ -0,0 +1,32 @@
+﻿using airsafenet_backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace airsafenet_backend.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { 
+        }
+
+        public DbSet<User> Users => Set<User>();
+        public DbSet<UserPreferences> UserPreferences => Set<UserPreferences>();
+        public DbSet<AirQualityLog> AirQualityLogs => Set<AirQualityLog>();
+        public DbSet<AlertLog> AlertLogs => Set<AlertLog>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>()
+                .HasIndex(x => x.Email)
+                .IsUnique();
+
+            modelBuilder.Entity<User>()
+                .HasOne(x => x.Preferences)
+                .WithOne(x => x.User)
+                .HasForeignKey<UserPreferences>(x => x.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
