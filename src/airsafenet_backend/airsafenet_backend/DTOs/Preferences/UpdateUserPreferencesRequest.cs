406f33a28e691fe7814da1760c35a86ab84bfc0f
NguyenTriBaoThang <nguyentribaothang@gmail.com>
2026-04-04 23:35:38 +0700

    feat(user-preferences): enhance preferences DTOs for update and response

--- src/airsafenet_backend/airsafenet_backend/DTOs/Preferences/UpdateUserPreferencesRequest.cs
+++ src/airsafenet_backend/airsafenet_backend/DTOs/Preferences/UpdateUserPreferencesRequest.cs
@@ -1,6 +1,16 @@
-﻿namespace airsafenet_backend.DTOs.Preferences
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace airsafenet_backend.DTOs.Preferences
 {
     public class UpdateUserPreferencesRequest
     {
+        [Required]
+        [MaxLength(50)]
+        public string UserGroup { get; set; } = "normal";
+
+        [MaxLength(150)]
+        public string PreferredLocation { get; set; } = "Ho Chi Minh City";
+
+        public bool NotifyEnabled { get; set; } = true;
     }
 }
--- src/airsafenet_backend/airsafenet_backend/DTOs/Preferences/UserPreferencesResponse.cs
+++ src/airsafenet_backend/airsafenet_backend/DTOs/Preferences/UserPreferencesResponse.cs
@@ -2,5 +2,10 @@
 {
     public class UserPreferencesResponse
     {
+        public int UserId { get; set; }
+        public string UserGroup { get; set; } = "normal";
+        public string PreferredLocation { get; set; } = "Ho Chi Minh City";
+        public bool NotifyEnabled { get; set; }
+        public DateTime UpdatedAt { get; set; }
     }
 }
