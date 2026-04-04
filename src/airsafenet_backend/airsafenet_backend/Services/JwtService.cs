1949cff7e2086a278c1bff18a4b4c4cf1e51b8c8
NguyenTriBaoThang <nguyentribaothang@gmail.com>
2026-04-04 23:43:21 +0700

    feat(dashboard): enhance dashboard summary response DTO

--- src/airsafenet_backend/airsafenet_backend/Services/JwtService.cs
+++ src/airsafenet_backend/airsafenet_backend/Services/JwtService.cs
@@ -1,4 +1,10 @@
-﻿namespace airsafenet_backend.Services
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using airsafenet_backend.Models;
+using Microsoft.IdentityModel.Tokens;
+
+namespace airsafenet_backend.Services
 {
     public class JwtService
     {
